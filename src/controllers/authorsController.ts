57fc0ba93e3ace0e9ed9f3cd7b04e616f444ecbf
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-09-13 09:00:05 -0300

    feature: add validations and paginations and filter

--- src/controllers/authorsController.ts
+++ src/controllers/authorsController.ts
@@ -2,70 +2,85 @@ import { NextFunction, Request, Response } from "express";
 import Authors from "../models/Author";
 import { IAuthor } from "../types/Author";
 import Helper from "../helper/Helper";
+import ErrorNotFound from "../errors/errorNotFound";
 
 class AuthorsController {
-	static getAll = (req: Request, res: Response, next: NextFunction) => {
-		Authors.find((error: any, authors: IAuthor) => {
-			if (error) {
-				next(error);
-			} else {
-				return res
-					.status(200)
-					.send(Helper.ResponseData(200, null, null, authors));
-			}
-		});
+	static getAll = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const allAuthors: IAuthor[] = await Authors.find();
+
+			return res
+				.status(200)
+				.send(Helper.ResponseData(200, null, null, allAuthors));
+		} catch (error) {
+			next(error);
+		}
 	};
 
-	static getOne = (req: Request, res: Response, next: NextFunction) => {
+	static getOne = async (req: Request, res: Response, next: NextFunction) => {
 		const id = req.params.id;
 
-		Authors.findOne({ _id: id }, (error: any, author: IAuthor) => {
-			if (error) {
-				next(error);
-			} else {
+		try {
+			const author: IAuthor | null = await Authors.findById(id);
+
+			if (author) {
 				return res
 					.status(200)
 					.send(Helper.ResponseData(200, null, null, author));
+			} else {
+				next(new ErrorNotFound("Id Author not found"));
 			}
-		});
+		} catch (error) {
+			next(error);
+		}
 	};
 
-	static create = (req: Request, res: Response, next: NextFunction) => {
+	static create = async (req: Request, res: Response, next: NextFunction) => {
 		const author = new Authors(req.body);
 
-		author.save((error) => {
-			if (error) {
-				next(error);
-			} else {
+		try {
+			await author.save();
+			return res
+				.status(201)
+				.send(Helper.ResponseData(201, null, null, author.toJSON()));
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	static update = async (req: Request, res: Response, next: NextFunction) => {
+		const id = req.params.id;
+
+		try {
+			const authorEdited = await Authors.findByIdAndUpdate(id, {
+				$set: req.body,
+			});
+
+			if (authorEdited) {
 				return res
-					.status(201)
-					.send(Helper.ResponseData(201, null, null, author.toJSON()));
+					.status(200)
+					.send(Helper.ResponseData(200, null, null, authorEdited));
+			} else {
+				next(new ErrorNotFound("Id Author not found"));
 			}
-		});
+		} catch (error) {
+			next(error);
+		}
 	};
 
-	static update = (req: Request, res: Response, next: NextFunction) => {
+	static delete = async (req: Request, res: Response, next: NextFunction) => {
 		const id = req.params.id;
 
-		Authors.findByIdAndUpdate(id, { $set: req.body }, (error: any) => {
-			if (error) {
-				next(error);
-			} else {
-				next(error);
-			}
-		});
-	};
-
-	static delete = (req: Request, res: Response, next: NextFunction) => {
-		const id = req.params.id;
-
-		Authors.findByIdAndDelete(id, (error: any) => {
-			if (error) {
-				next(error);
-			} else {
+		try {
+			const authorDeleted = await Authors.findByIdAndDelete(id);
+			if (authorDeleted) {
 				return res.status(200).send(Helper.ResponseData(200, null, null, null));
+			} else {
+				next(new ErrorNotFound("Id Author not found"));
 			}
-		});
+		} catch (error) {
+			next(error);
+		}
 	};
 }
 
--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -2,96 +2,156 @@ import { NextFunction, Request, Response } from "express";
 import Books from "../models/Book";
 import { IBook } from "../types/Book";
 import Helper from "../helper/Helper";
+import ErrorNotFound from "../errors/errorNotFound";
+import { Authors } from "../models";
+import BadRequest from "../errors/badRequest";
 
 class BooksController {
-	static getAll = (req: Request, res: Response, next: NextFunction) => {
-		Books.find()
-			.populate("author")
-			.exec((error: any, books: IBook[]) => {
-				if (error) {
-					next(error);
-				} else {
-					return res
-						.status(200)
-						.send(Helper.ResponseData(200, null, null, books));
-				}
-			});
+	static getAll = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const allBooks: IBook[] = await Books.find().sort({ title: 1 }).exec();
+
+			return res
+				.status(200)
+				.send(Helper.ResponseData(200, null, null, allBooks));
+		} catch (error) {
+			next(error);
+		}
 	};
 
-	static getOne = (req: Request, res: Response, next: NextFunction) => {
-		const id = req.params.id;
-
-		Books.findOne({ _id: id })
-			.populate("author", "name")
-			.exec((error: any, book: IBook | null) => {
-				if (error) {
-					next(error);
-				} else {
-					return res
-						.status(200)
-						.send(Helper.ResponseData(200, null, null, book));
-				}
-			});
-	};
-
-	static create = (req: Request, res: Response, next: NextFunction) => {
-		const book = new Books(req.body);
-
-		book.save((error: any) => {
-			if (error) {
-				next(error);
-			} else {
-				return res
-					.status(201)
-					.send(Helper.ResponseData(201, null, null, book.toJSON()));
-			}
-		});
-	};
-
-	static update = (req: Request, res: Response, next: NextFunction) => {
-		const id = req.params.id;
-
-		Books.findByIdAndUpdate(id, { $set: req.body }, (error: any) => {
-			if (error) {
-				next(error);
-			} else {
-				return res.status(200).send(Helper.ResponseData(200, null, null, null));
-			}
-		});
-	};
-
-	static delete = (req: Request, res: Response, next: NextFunction) => {
-		const id = req.params.id;
-
-		Books.findByIdAndDelete(id, (error: any) => {
-			if (error) {
-				next(error);
-			} else {
-				return res.status(200).send(Helper.ResponseData(200, null, null, null));
-			}
-		});
-	};
-
-	static getByPublishingCompany = (
+	static getPagination = async (
 		req: Request,
 		res: Response,
 		next: NextFunction
 	) => {
-		const publishingCompany = req.params.publishingCompany;
+		try {
+			const { limit = 1, page = 1 }: any = req.query;
 
-		Books.findOne(
-			{ publishingCompany: publishingCompany },
-			{},
-			(error: any, books: IBook[]) => {
-				if (error) {
-					next(error);
+			const limitParsed = parseInt(limit);
+			const pageParsed = parseInt(page);
+
+			if (limitParsed > 0 && pageParsed > 0) {
+				const booksPaginated = await Books.find()
+					.skip((pageParsed - 1) * limitParsed)
+					.sort({ title: 1 })
+					.limit(limitParsed)
+					.exec();
+
+				return res
+					.status(200)
+					.send(Helper.ResponseData(200, null, null, booksPaginated));
+			} else {
+				next(new BadRequest());
+			}
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	static getOne = async (req: Request, res: Response, next: NextFunction) => {
+		const id = req.params.id;
+
+		try {
+			const book: IBook | null = await Books.findOne({ _id: id }).exec();
+
+			if (book) {
+				return res.status(200).send(Helper.ResponseData(200, null, null, book));
+			} else {
+				next(new ErrorNotFound("Id Book not found"));
+			}
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	static create = async (req: Request, res: Response, next: NextFunction) => {
+		const book = new Books(req.body);
+
+		try {
+			await book.save();
+			return res
+				.status(201)
+				.send(Helper.ResponseData(201, null, null, book.toJSON()));
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	static update = async (req: Request, res: Response, next: NextFunction) => {
+		const id = req.params.id;
+
+		try {
+			const bookEdited = await Books.findByIdAndUpdate(id, { $set: req.body });
+
+			if (bookEdited) {
+				return res
+					.status(200)
+					.send(Helper.ResponseData(200, null, null, bookEdited));
+			} else {
+				next(new ErrorNotFound("Id Book not found"));
+			}
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	static delete = async (req: Request, res: Response, next: NextFunction) => {
+		const id = req.params.id;
+
+		try {
+			const bookDeleted = await Books.findByIdAndDelete(id);
+
+			if (bookDeleted) {
+				return res.status(200).send(Helper.ResponseData(200, null, null, null));
+			} else {
+				next(new ErrorNotFound("Id Book not found"));
+			}
+		} catch (error) {
+			next(error);
+		}
+	};
+
+	static getByFilter = async (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		try {
+			const { publishingCompany, title, nameAuthor } = req.query;
+			const searchParams: any = {};
+
+			if (publishingCompany) {
+				searchParams.publishingCompany = publishingCompany;
+			}
+
+			if (title) {
+				const regexTitle = new RegExp(`${title}`, "i");
+				searchParams.title = regexTitle;
+			}
+
+			if (nameAuthor) {
+				const author = await Authors.findOne({ name: nameAuthor });
+
+				if (author) {
+					const authorId = author._id;
+					searchParams.author = authorId;
 				} else {
-					return res
-						.status(200)
-						.send(Helper.ResponseData(200, null, null, books));
+					res.status(200).send([]);
 				}
 			}
-		);
+
+			const booksFiltered = await Books.find(searchParams);
+
+			if (booksFiltered) {
+				return res
+					.status(200)
+					.send(Helper.ResponseData(200, null, null, booksFiltered));
+			} else {
+				next(new ErrorNotFound("Book not found"));
+			}
+		} catch (error) {
+			next(error);
+		}
 	};
 }
 
