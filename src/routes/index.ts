e733060240833b8141d29a74268f8ed3913edc10
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-09-13 09:00:13 -0300

    feature: add routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,22 @@
-import express from 'express'
-import AuthorsController from '../controllers/authorsController'
-import BooksController from '../controllers/booksController'
+import express from "express";
+import AuthorsController from "../controllers/authorsController";
+import BooksController from "../controllers/booksController";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/authors', AuthorsController.getAll)
-router.get('/authors/:id', AuthorsController.getOne)
-router.post('/authors', AuthorsController.create)
-router.put('/authors/:id', AuthorsController.update)
-router.delete('/authors/:id', AuthorsController.delete)
+router.get("/authors", AuthorsController.getAll);
+router.get("/authors/:id", AuthorsController.getOne);
+router.post("/authors", AuthorsController.create);
+router.put("/authors/:id", AuthorsController.update);
+router.delete("/authors/:id", AuthorsController.delete);
 
-router.get('/books', BooksController.getAll)
-router.get('/books/search', BooksController.getByPublishingCompany)
-router.post('/books', BooksController.create)
-router.get('/books/:id', BooksController.getOne)
-router.put('/books/:id', BooksController.update)
-router.delete('/books/:id', BooksController.delete)
+router.get("/books", BooksController.getAll);
+router.get("/books/pagination", BooksController.getPagination);
 
-export default router
+router.get("/books/search", BooksController.getByFilter);
+router.get("/books/:id", BooksController.getOne);
+router.post("/books", BooksController.create);
+router.put("/books/:id", BooksController.update);
+router.delete("/books/:id", BooksController.delete);
+
+export default router;
